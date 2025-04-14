bc54bf1d284783b5cec8e18955bfe17ebab646f6
kosaraju3333 <88775633+kosaraju3333@users.noreply.github.com>
2025-04-14 15:47:44 +0530

    Create spontansolutions-bank-app.sh

---
+++ spontansolutions-bank-app.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+#Downloading bank app jar from AWS S3
+
+working_dir=/home/ubuntu/bank-app
+
+#Creating /home/ubuntu/portal-spontansolutions directory if not present
+
+if [ -d "$working_dir" ];
+then
+echo "$working_dir is already exist"
+else
+echo "********** creating $working_dir directory **********"
+mkdir $working_dir
+fi
+
+#Downloading bank app artifactory from AWS S3
+
+echo "********** Downloading bank app artifactory from AWS S3 **********"
+aws s3 cp s3://bank-app-spontansolutions/bankapp-0.0.1-SNAPSHOT.jar $working_dir
+
+#Starting the Bank app up from jar
+
+echo "********** Staring  bank app service from jar **********"
+sudo bash -c 'nohup java -jar /home/ubuntu/bank-app/bankapp-0.0.1-SNAPSHOT.jar >> /var/log/bank-app/app.log 2>&1 &'
